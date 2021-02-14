1b41f01a55dd61c9b3bea289517f83f5aea2cae0
gaegulgaegul <dla9721@gmail.com>
2021-02-14 17:09:54 +0900

    Spring Web MVC intro

---
+++ inflearn-spring-boot/devtools-spring-boot-starter/src/main/java/me/whiteship/devtoolsspringbootstarter/sample/SampleService.java
@@ -0,0 +1,2 @@
+package me.whiteship.devtoolsspringbootstarter.sample;public class SampleService {
+}
---
+++ inflearn-spring-boot/webmvc-spring-boot-starter/src/main/java/me/whiteship/webmvcspringbootstarter/user/UserController.java
@@ -0,0 +1,2 @@
+package me.whiteship.webmvcspringbootstarter.user;public class UserController {
+}
