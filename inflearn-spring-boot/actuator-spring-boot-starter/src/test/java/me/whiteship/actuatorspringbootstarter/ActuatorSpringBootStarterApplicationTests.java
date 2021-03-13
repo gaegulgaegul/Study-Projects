7174d06ac094e1ed657d5625d0258258a6367028
gaegulgaegul <dla9721@gmail.com>
2021-03-13 14:48:26 +0900

    Spring Boot Actuator

---
+++ inflearn-spring-boot/actuator-spring-boot-starter/src/test/java/me/whiteship/actuatorspringbootstarter/ActuatorSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.actuatorspringbootstarter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ inflearn-spring-boot/admin-spring-boot-starter/src/test/java/me/whiteship/adminspringbootstarter/AdminSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.adminspringbootstarter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
