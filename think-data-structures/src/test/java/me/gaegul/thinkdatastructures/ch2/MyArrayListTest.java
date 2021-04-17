0dc27d3822ad88d3ec8d64259ce10d7ef53b2fe2
gaegulgaegul <dla9721@gmail.com>
2021-04-17 18:30:28 +0900

    코드 정리

--- think-data-structures/src/test/java/me/gaegul/thinkdatastructures/ch2/MyArrayListTest.java
+++ think-data-structures/src/test/java/me/gaegul/thinkdatastructures/ch2/MyArrayListTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MyArrayListTest {
 
