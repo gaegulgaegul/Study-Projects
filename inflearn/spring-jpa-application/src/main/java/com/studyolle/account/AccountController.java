f69813614ea4dfcfd263f76d90408f6362f530bc
gaegulgaegul <dla9721@gmail.com>
2021-04-25 13:15:27 +0900

    5장 이중 연결 리스트

--- inflearn/spring-jpa-application/src/main/java/com/studyolle/account/AccountController.java
+++ inflearn/spring-jpa-application/src/main/java/com/studyolle/account/AccountController.java
@@ -1,9 +1,6 @@
 package com.studyolle.account;
 
-import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
